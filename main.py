cda934fc33d1982e617326a5d8e71037c42b356e
Kokonin <98752478+Kokonin@users.noreply.github.com>
2022-06-14 15:46:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+from turtle import color
+from colorama import colorama_text
+import colorama
+from pygame import Color
+
+num = random.randint(0, 100000)
+
+print(colorama.Fore.YELLOW + "Generated number = %s" %(num))
