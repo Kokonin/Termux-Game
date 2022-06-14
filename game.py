4e1cfb66e5467900a39225b52eff0b92a11b693b
Kokonin <98752478+Kokonin@users.noreply.github.com>
2022-06-14 16:13:44 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,5 @@
 import random
-from colorama import colorama_text
-import colorama
 
 num = random.randint(0, 100000)
 
-print(colorama.Fore.GREEN + "Generated number = %s" %(num))
+print("Generated number = %s" %(num))
